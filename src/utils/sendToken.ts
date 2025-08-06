a5a3f78b83395aa93d674f55c2542917fd854a0d
abhijith <abhijithajith13@gmail.com>
2025-08-06 15:51:49 +0530

    cookie error

--- src/utils/sendToken.ts
+++ src/utils/sendToken.ts
@@ -12,6 +12,7 @@ const sendTokenResponse = (
   const cookieOptions = {
     httpOnly: true,
     sameSite: "strict" as const,
+    secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
